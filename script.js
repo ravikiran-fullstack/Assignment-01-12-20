390e80b209767a20a2df9fed66a9e7399f49c536
kiran-code <ravikiransjce.code@gmail.com>
2020-12-01 22:28:20 +0530

    moved all js code to script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function countriesApi(url) {
+  return new Promise((res, rej) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        res(xhr.responseText);
+      } else if (this.readyState == 4 && this.status !== 200) {
+        rej("error occurred while fetching data");
+      }
+    };
+
+    xhr.send();
+  });
+}
+
+let url = "https://restcountries.eu/rest/v2/all";
+countriesApi(url)
+  .then((response) => {
+    generateHtml(JSON.parse(response));
+  })
+  .catch((err) => console.error(err));
+
+
+// DOM 
 //Creates individual Card
 function createCard(countryObj) {
   const card = createDomElement("div", "card");

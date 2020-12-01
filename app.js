a0178bd168d59500be7d5aa182a186b402b1cf5b
kiran-code <ravikiransjce.code@gmail.com>
2020-12-01 19:46:06 +0530

    Finished

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// fetch('https://restcountries.eu/rest/v2/all')
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-
-function getCountriesData(url) {
+function countriesApi(url) {
   return new Promise((res, rej) => {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
@@ -12,7 +7,7 @@ function getCountriesData(url) {
       if (this.readyState == 4 && this.status == 200) {
         res(xhr.responseText);
       } else if (this.readyState == 4 && this.status !== 200) {
-        rej("error");
+        rej("error occurred while fetching data");
       }
     };
 
@@ -21,34 +16,8 @@ function getCountriesData(url) {
 }
 
 let url = "https://restcountries.eu/rest/v2/all";
-
-//console.log(getCountriesData());
-getCountriesData(url)
+countriesApi(url)
   .then((response) => {
-    console.log(response);
+    generateHtml(JSON.parse(response));
   })
   .catch((err) => console.error(err));
-
-// function xmlHttp(url){
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("GET", url, true);
-//   xhr.send();
-//   xhr.onload = function(){
-//     console.log(xhr.response);
-//   }
-//   xhr.onerror = function(){
-//     console.log('error');
-//   }
-
-//   xhr.onreadystatechange = function () {
-//     var _data = this;
-//     if (this.readyState == 4 && this.status == 200) {
-//        console.log(xhr.response);
-//     } else if (this.readyState == 4 && this.status !== 200) {
-//       console.error('error');
-//     }
-//   };
-
-// }
-
-// xmlHttp(url);
---
+++ dom.js
@@ -0,0 +1,83 @@
+//Creates individual Card
+function createCard(countryObj) {
+  const card = createDomElement("div", "card");
+    const cardBody = createDomElement("div", "card-body");
+      // If the name of the country is too long change the font size
+      const cardTitle = createDomElement("h5", "card-title");
+      if (countryObj.name.length > 20) {
+        cardTitle.classList.add("short-title");
+      }
+      cardTitle.innerHTML = countryObj.name;
+
+      const image = createDomElement("img", "card-img-top");
+        image.src = countryObj.flag;
+        image.alt = countryObj.name;
+        
+      const cardContents = createDomElement("div", "card-contents");
+
+        const capitalP = createDomElement("p", "capital");
+          capitalP.innerHTML = "Capital:";
+          const capitalPSpan = createDomElement("span");
+          if (!countryObj.capital) {
+            capitalPSpan.innerHTML = "NA";
+          } else {
+            capitalPSpan.innerHTML = countryObj.capital;
+          }
+        capitalP.append(capitalPSpan);
+
+        const countryCodesP = createDomElement("p");
+          countryCodesP.innerHTML = "Country Codes: ";
+          const countryCodesPSpan = createDomElement("span");
+          countryCodesPSpan.innerHTML = `${countryObj.alpha2Code}, ${countryObj.alpha3Code}`;
+        countryCodesP.append(countryCodesPSpan);
+
+        const regionP = createDomElement("p");
+          regionP.innerHTML = "Region:";
+          const regionPSpan = createDomElement("span");
+          regionPSpan.innerHTML = countryObj.region;
+        regionP.append(regionPSpan);
+
+        const latLongP = createDomElement("p");
+          latLongP.innerHTML = "Lat Long:";
+          const latLongPSpan = createDomElement("span");
+          latLongPSpan.innerHTML = formatLatLng(countryObj.latlng);
+        latLongP.append(latLongPSpan);
+
+      cardContents.append(capitalP, countryCodesP, regionP, latLongP);
+      cardBody.append(cardTitle, image, cardContents);
+    card.append(cardBody);
+  return card;
+}
+
+// Formats the latitude and longitude
+function formatLatLng(latLngArr) {
+  return latLngArr.map((ele) => ele.toFixed(2)).join(",");
+}
+
+// Creates a Dom element and assigns class and id to it, if they are not empty
+function createDomElement(ele, eleClass = "", eleId = "") {
+  const element = document.createElement(ele);
+  if (eleClass !== "") {
+    element.setAttribute("class", eleClass);
+  }
+  if (eleId !== "") {
+    element.setAttribute("id", eleId);
+  }
+  return element;
+}
+
+// Generates Body of the document
+function generateHtml(countriesInfo) {
+  const container = createDomElement("div", "container-fluid");
+    const row = createDomElement("div", "row");
+      const column = createDomElement("div", "col-12 countriesInfo");
+
+      countriesInfo.forEach((country) => {
+        const card = createCard(country);
+        column.append(card);
+      });
+
+    row.append(column);
+    container.append(row);
+  document.body.append(container);
+}
